0c418c32eae9479183ead877f4616e4c6362c1fa
M M Aftab Chowdhury <mmaftab.chowdhury@cgi.com>
2021-11-30 22:47:46 +0100

    master-implement-toastr-notification>> delete default code from app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToastrComponent } from './components/toastr/toastr.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToastrComponent
   ],
   imports: [
     BrowserModule,
